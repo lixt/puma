f3d95c0919269adadd5266754cbaabfad63f708e
xiaotian.li <xiaotian.li@dianping.com>
2016-01-29 17:51:31 +0800

    add puma-common and puma-manage-core module

---
+++ puma-common/src/main/java/com/dianping/puma/common/PumaLifeCycle.java
@@ -0,0 +1,12 @@
+package com.dianping.puma.common;
+
+/**
+ * Created by xiaotian.li on 16/1/19.
+ * Email: lixiaotian07@gmail.com
+ */
+public interface PumaLifeCycle {
+
+    void start();
+
+    void stop();
+}
